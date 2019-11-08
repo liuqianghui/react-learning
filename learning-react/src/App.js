0099d63939062e39eea27e6b45cd060cd6a65515
Liuqh <lqh9428@gmail.com>
2019-11-08 20:42:15 +0800

    auto commit

--- learning-react/src/App.js
+++ learning-react/src/App.js
@@ -1,5 +1,12 @@
 import React from 'react'
-import logo from './logo.svg'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from 'react-router-dom'
 import './App.css'
 
 function Square(props) {
@@ -80,6 +87,10 @@ class Game extends React.Component {
       xIsNext: step % 2 === 0
     })
   }
+
+  handleSubmit(params) {
+    console.log(11)
+  }
   render() {
     const history = this.state.history
     const current = history[this.state.stepNumber]
@@ -99,6 +110,11 @@ class Game extends React.Component {
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
     }
+    const comments = [
+      { username: 'Jerry', content: 'Hello' },
+      { username: 'Tomy', content: 'World' },
+      { username: 'Lucy', content: 'Good' }
+    ]
     return (
       <div className="game">
         <div className="game-board">
@@ -108,11 +124,89 @@ class Game extends React.Component {
           <div>{status}</div>
           <ol>{moves}</ol>
         </div>
+        <button className="submit" onClick={this.handleSubmit.bind(this)}>
+          hello
+        </button>
+        <div>
+          {comments.map((comment, i) => {
+            return <p key={i}>{comment.username}</p>
+          })}
+        </div>
+        <Router>
+          <div>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/topics">Topics</Link>
+              </li>
+            </ul>
+
+            <Switch>
+              <Route path="/about">
+                <About />
+              </Route>
+              <Route path="/topics">
+                <Topics />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
       </div>
     )
   }
 }
+function Home() {
+  return <h2>Home</h2>
+}
 
+function About() {
+  return <h2>About</h2>
+}
+
+function Topics() {
+  let match = useRouteMatch()
+
+  return (
+    <div>
+      <h2>Topics</h2>
+
+      <ul>
+        <li>
+          <Link to={`${match.url}/components`}>Components</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+        </li>
+      </ul>
+
+      {/* The Topics page has its own <Switch> with more routes
+          that build on the /topics URL path. You can think of the
+          2nd <Route> here as an "index" page for all topics, or
+          the page that is shown when no topic is selected */}
+      <Switch>
+        <Route path={`${match.path}/:topicId`}>
+          <Topic />
+        </Route>
+        <Route path={match.path}>
+          <h3>Please select a topic.</h3>
+        </Route>
+      </Switch>
+    </div>
+  )
+}
+
+function Topic() {
+  let { topicId } = useParams()
+  return <h3>Requested topic ID: {topicId}</h3>
+}
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
